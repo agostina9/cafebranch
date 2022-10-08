aacebc1ac6f43a7a255a2084f9c4e2f49453840b
Agostina <lozagostina@gmail.com>
2022-10-08 19:45:21 -0300

    agrego administrador e item

--- src/components/views/Administrador.jsx
+++ src/components/views/Administrador.jsx
@@ -1,8 +1,30 @@
+import { Button, Container, Table } from "react-bootstrap";
+import ItemProducto from "./producto/ItemProducto";
+
 const Administrador = () => {
     return (
-        <div>
-            <h1>este es el administrador</h1>
+      <Container>
+        <div className="d-flex justify-content-between">
+        <h2 className="display-4 mt-4">Productos disponibles</h2>
+        <Button>Agregar</Button>
         </div>
+        <hr></hr>
+        <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Cod</th>
+          <th>Producto</th>
+          <th>Precio</th>
+          <th>URL de Imagen</th>
+          <th>Categoria</th>
+          <th>Opciones</th>
+        </tr>
+      </thead>
+      <tbody>
+     <ItemProducto></ItemProducto>
+      </tbody>
+    </Table>
+      </Container>
     );
 };
 
